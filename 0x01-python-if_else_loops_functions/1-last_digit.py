09053d516adb3b83a01712e12c0562ece552edb0
kamsograce <gracekamso@mail.com>
2022-11-30 08:15:39 -0800

    Just Commited

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"{last_digit} and is 0")
 else:
-    print(f"{remainder} and is less than 6 and not 0")
+    print(f"{last_digit} and is less than 6 and not 0")
