ac41bfb0aa5988d06c73ab0de34dade027b522e1
kamsograce <gracekamso@mail.com>
2022-12-05 08:37:12 -0800

    Just Committed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end="")
-            else:
-                print("{:d}".format(matrix[i][j]), end=" ")
-                print("")
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
