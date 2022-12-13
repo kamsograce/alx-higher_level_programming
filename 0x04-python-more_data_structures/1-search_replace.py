011942e11b29c607e467af08edc12fbf8784b97b
kamsograce <gracekamso@mail.com>
2022-12-13 05:49:09 -0800

    Just Committed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-     new_list = my_list[:]
-     for i in range(len(new_list)):
-         if new_list[i] == search:
-             new_list[i] = replace
-    return new_list
+    return [i if i is not = search else replace for i in my_list]
